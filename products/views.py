599e637f3e5310f9981507c04e6822f6539b545c
irka775 <barbascumpa54@gmail.com>
2025-01-27 17:07:32 +0000

    Add function in views to chheck acces to aws

--- products/views.py
+++ products/views.py
@@ -7,7 +7,48 @@ from django.db.models.functions import Lower
 from .models import Product, Category
 from .forms import ProductForm
 
-# Create your views here.
+from django.core.files.storage import default_storage
+from django.core.files.base import ContentFile
+
+
+
+
+
+
+
+
+
+
+
+
+
+def test_s3_write():
+    try:
+        file_name = "staticfiles/test.txt"
+        content = ContentFile(b"Hello from Django!")
+        file_path = default_storage.save(file_name, content)
+        return f"File saved to {file_path}"
+    except Exception as e:
+        return f"Error: {e}"
+
+
+def test_s3_read():
+    try:
+        file_name = "staticfiles/test.txt"
+        with default_storage.open(file_name, "r") as file:
+            content = file.read()
+            print(content)
+        return f"File content: {content}"
+    except Exception as e:
+        return f"Error: {e}"
+
+
+
+
+
+
+
+
 
 def all_products(request):
     """ A view to show all products, including sorting and search queries """
@@ -32,7 +73,7 @@ def all_products(request):
                 if direction == 'desc':
                     sortkey = f'-{sortkey}'
             products = products.order_by(sortkey)
-            
+
         if 'category' in request.GET:
             categories = request.GET['category'].split(',')
             products = products.filter(category__name__in=categories)
@@ -41,10 +82,12 @@ def all_products(request):
         if 'q' in request.GET:
             query = request.GET['q']
             if not query:
-                messages.error(request, "You didn't enter any search criteria!")
+                messages.error(
+                    request, "You didn't enter any search criteria!")
                 return redirect(reverse('products'))
-            
-            queries = Q(name__icontains=query) | Q(description__icontains=query)
+
+            queries = Q(name__icontains=query) | Q(
+                description__icontains=query)
             products = products.filter(queries)
 
     current_sorting = f'{sort}_{direction}'
@@ -70,6 +113,7 @@ def product_detail(request, product_id):
 
     return render(request, 'products/product_detail.html', context)
 
+
 def test(request):
     """ A view to show individual product details """
 
@@ -90,10 +134,11 @@ def add_product(request):
             messages.success(request, 'Successfully added product!')
             return redirect(reverse('product_detail', args=[product.id]))
         else:
-            messages.error(request, 'Failed to add product. Please ensure the form is valid.')
+            messages.error(
+                request, 'Failed to add product. Please ensure the form is valid.')
     else:
         form = ProductForm()
-        
+
     template = 'products/add_product.html'
     context = {
         'form': form,
@@ -117,7 +162,8 @@ def edit_product(request, product_id):
             messages.success(request, 'Successfully updated product!')
             return redirect(reverse('product_detail', args=[product.id]))
         else:
-            messages.error(request, 'Failed to update product. Please ensure the form is valid.')
+            messages.error(
+                request, 'Failed to update product. Please ensure the form is valid.')
     else:
         form = ProductForm(instance=product)
         messages.info(request, f'You are editing {product.name}')
