c1cfc3c6e6e4608ad2fa1ba1d7898468da3771bc
irka775 <barbascumpa54@gmail.com>
2025-01-29 16:33:49 +0000

    Improve security: Only admins can edit store settings & show error for others

--- store_settings/views.py
+++ store_settings/views.py
@@ -4,19 +4,22 @@ from django.contrib.auth.decorators import login_required
 from .models import StoreSettings
 from .forms import StoreSettingsForm
 
-
 @login_required
 def store_settings_view(request):
-    # Get existing settings or create a default one
+    # Obține setările existente
     settings = StoreSettings.objects.first()
+    form = StoreSettingsForm(instance=settings)
+
     if request.method == "POST":
+        if not request.user.is_superuser:  
+            messages.error(request, "You do not have permission to update store settings.")
+            return redirect("store_settings")
+
         form = StoreSettingsForm(request.POST, request.FILES, instance=settings)
         if form.is_valid():
             form.save()
             messages.success(request, "Settings updated successfully!")
             return redirect("store_settings")
-    else:
-        form = StoreSettingsForm(instance=settings)
 
     context = {"form": form}
     return render(request, "store_settings/store_settings.html", context)
