667400c27bbd46373aa09b5e5586530a8ee56792
irka775 <barbascumpa54@gmail.com>
2025-02-08 19:32:17 +0000

    Deploy direct pe Heroku for debuging

--- store_settings/admin.py
+++ store_settings/admin.py
@@ -9,17 +9,17 @@ class StoreSettingsAdmin(admin.ModelAdmin):
         "store_name",
         "contact_email",
         "currency",
-        "enable_maintenance_mode")
+        "enable_maintenance_mode"
+    )
     list_editable = ("enable_maintenance_mode",)
     fieldsets = (
         ("General Settings", {"fields": ("store_name", "store_logo", "main_page_image", "welcome_msg",)}),
         ("Contact Info", {"fields": ("contact_email", "contact_phone")}),
-        ("E-Commerce Settings", {"fields": ("currency", "shipping_options")}),
-        ("Payment Methods", {"fields": ("enable_paypal",
-         "enable_stripe", "enable_cash_on_delivery")}),
-        ("Additional Options", {
-         "fields": ("enable_reviews", "enable_maintenance_mode")}),
+        ("E-Commerce Settings", {"fields": ("currency",)}),  
+        ("Payment Methods", {"fields": ("enable_paypal", "enable_stripe", "enable_cash_on_delivery")}),
+        ("Additional Options", {"fields": ("enable_reviews", "enable_maintenance_mode")}),
     )
+
 
 
 @admin.register(ShippingSettings)
