9ccb7719e7a793113acf5af7748d761a182bde86
irka775 <barbascumpa54@gmail.com>
2025-02-08 19:20:53 +0000

    Deploy direct pe Heroku

--- store_settings/middlewares.py
+++ store_settings/middlewares.py
@@ -1,15 +1,27 @@
 from django.shortcuts import HttpResponse
 from store_settings.models import StoreSettings
-from little_explorers_p.context_processors import StoreSettings
+
 class MaintenanceModeMiddleware:
-    """Middleware pentru activarea modului de mentenanță."""
+    """Middleware for maintenance mode."""
+
     def __init__(self, get_response):
         self.get_response = get_response
 
-
     def __call__(self, request):
-        store_settings = StoreSettings.objects.filter(user__is_superuser=True).first() and not request.path.startswith("/admin/")
-        if store_settings and store_settings.enable_maintenance_mode and not request.user.is_superuser:
-            return HttpResponse("<h1>⚠️ Site-ul este în mentenanță ⚠️</h1><p>Vă rugăm să reveniți mai târziu.</p>", status=503)
+        store_settings = StoreSettings.objects.filter(user__is_superuser=True).first()
+        
+        if (
+            store_settings
+            and store_settings.enable_maintenance_mode
+            and not request.user.is_superuser
+            and not request.path.startswith("/admin/")
+        ):
+            return HttpResponse(
+                "<h1>⚠️ Maintenance Mode Enabled ⚠️</h1>"
+                "<p>Our website is currently undergoing maintenance.</p>"
+                "<p>We apologize for the inconvenience and appreciate your patience.</p>"
+                "<p>Please check back later.</p>",
+                status=503,
+            )
 
         return self.get_response(request)
