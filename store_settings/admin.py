c5431686d604de262f9ad18a7f86dfad4bddc0c2
irka775 <barbascumpa54@gmail.com>
2025-02-10 20:35:07 +0000

    Make store settings common for all admins

--- store_settings/admin.py
+++ store_settings/admin.py
@@ -6,23 +6,32 @@ from .models import ShippingSettings, StoreSettings, Subscriber
 
 @admin.register(StoreSettings)
 class StoreSettingsAdmin(admin.ModelAdmin):
+    
     list_display = (
+        "user",
         "store_name",
+        "is_superuser",
+        "pk",
         "contact_email",
         "currency",
         "enable_maintenance_mode"
     )
     list_editable = ("enable_maintenance_mode",)
+
     fieldsets = (
-        ("General Settings", {"fields": ("store_name",
-         "store_logo", "main_page_image", "welcome_msg",)}),
+        ("General Settings", {"fields": ("store_name", "store_logo", "main_page_image", "welcome_msg",)}),
         ("Contact Info", {"fields": ("contact_email", "contact_phone")}),
         ("E-Commerce Settings", {"fields": ("currency",)}),
-        ("Payment Methods", {"fields": ("enable_paypal",
-         "enable_stripe", "enable_cash_on_delivery")}),
-        ("Additional Options", {
-         "fields": ("enable_reviews", "enable_maintenance_mode")}),
+        ("Payment Methods", {"fields": ("enable_paypal", "enable_stripe", "enable_cash_on_delivery")}),
+        ("Additional Options", {"fields": ("enable_reviews", "enable_maintenance_mode")}),
     )
+
+    def is_superuser(self, obj):
+        """Returnează True dacă utilizatorul asociat este superuser."""
+        return obj.user.is_superuser if obj.user else False
+
+    is_superuser.boolean = True  # Afișează iconița ✔️/❌ în Django Admin
+    is_superuser.short_description = "Superuser"  # Numele coloanei în Admin
 
 
 @admin.register(ShippingSettings)
